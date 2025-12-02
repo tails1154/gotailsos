12805312baf5173f3756880bf470024eb36d88eb
Timothy Heironimus <timothyheironimus6+github@gmail.com>
2025-12-02 00:20:16 -0600

    ai is taking us over slowly but surely

---
+++ tailsfs.cs
@@ -0,0 +1,10 @@
+namespace testOS.Filesystem {
+    // This file was coded without the help of a LLM. The only LLM powered parts would be me being too lazy to fix syntax errors and asking it to do it for me or not knowing how to do something (but not like teling it to make a entire filesystem like i did with BadFS*). TailsFS2 will probably be AI Assisted to fix FS issues. I'm stil learning here lol.
+    public class TailsFS(BlockDevice device) {
+         private BlockDevice device;
+
+        public Detect() {
+
+        }
+    }
+}
